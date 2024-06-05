e4438bcede44b1c88b5ebdc1bd88efb716d061d5
z1qnezt <misha232dog@gmail.com>
2024-06-06 00:32:49 +0300

    task4

--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
-#include "shape.h"
-#include "circle.h"
-#include "square.h"
+#include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
 #include <iostream>
 
 struct CircleDrawer {
